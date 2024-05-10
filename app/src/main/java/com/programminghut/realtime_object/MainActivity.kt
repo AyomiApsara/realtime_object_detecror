f71debb8ccc827d49d2a1956ea12c7666e6e324a
AyomiApsara <ayomi003apsara@gmail.com>
2024-05-10 18:54:29 +0530

    object detect mobile app full project

---
+++ app/src/main/java/com/programminghut/realtime_object/MainActivity.kt
@@ -0,0 +1,191 @@
+
+
+package com.programminghut.realtime_object
+
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.pm.PackageManager
+import android.graphics.*
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
+import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.util.Log
+import android.util.Range
+import android.view.Surface
+import android.view.TextureView
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.programminghut.realtime_object.ml.ObjectRiceDetect
+import org.tensorflow.lite.support.common.FileUtil
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var labels: List<String>
+    var colors = listOf<Int>(
+        Color.BLUE, Color.GREEN, Color.RED, Color.CYAN, Color.GRAY, Color.BLACK,
+        Color.DKGRAY, Color.MAGENTA, Color.YELLOW, Color.RED
+    )
+    val paint = Paint()
+    lateinit var imageProcessor: ImageProcessor
+    lateinit var bitmap: Bitmap
+    lateinit var imageView: ImageView
+    lateinit var cameraDevice: CameraDevice
+    lateinit var handler: Handler
+    lateinit var cameraManager: CameraManager
+    lateinit var textureView: TextureView
+    lateinit var model: ObjectRiceDetect
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        get_permission()
+
+        labels = FileUtil.loadLabels(this, "labels.txt")
+        imageProcessor = ImageProcessor.Builder().add(ResizeOp(320, 320, ResizeOp.ResizeMethod.BILINEAR)).build()
+        model = ObjectRiceDetect.newInstance(this)
+        val handlerThread = HandlerThread("videoThread")
+        handlerThread.start()
+        handler = Handler(handlerThread.looper)
+
+        imageView = findViewById(R.id.imageView)
+
+        textureView = findViewById(R.id.textureView)
+        textureView.surfaceTextureListener = object : TextureView.SurfaceTextureListener {
+            override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+                open_camera()
+            }
+
+            override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+            }
+
+            override fun onSurfaceTextureDestroyed(p0: SurfaceTexture): Boolean {
+                return false
+            }
+
+            override fun onSurfaceTextureUpdated(p0: SurfaceTexture) {
+                Log.d(TAG, "Surface texture updated")
+                bitmap = textureView.bitmap!!
+                var image = TensorImage.fromBitmap(bitmap)
+                image = imageProcessor.process(image)
+
+                val outputs = model.process(image)
+                val detectionResults = outputs.detectionResultList // Get the list of detection results
+
+                Log.d(TAG, "Number of detections: ${detectionResults.size}")
+
+                var mutable = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+                val canvas = Canvas(mutable)
+
+                val h = mutable.height.toFloat()
+                val w = mutable.width.toFloat()
+                paint.textSize = h / 20f
+                paint.strokeWidth = h / 85f
+
+                for (detectionResult in detectionResults) {
+                    val score = detectionResult.scoreAsFloat
+                    val location = detectionResult.locationAsRectF
+                    val category = detectionResult.categoryAsString
+
+                    // Process score, location, and category as needed
+                    if (score > 0.6) {
+                        val index = labels.indexOf(category)
+                        if (index != -1) { // Check if category exists in labels
+                            paint.color = colors[index % colors.size]
+                            paint.style = Paint.Style.STROKE
+
+                            val scaleFactorX = w / 320
+                            val scaleFactorY = h / 320
+
+                            canvas.drawRect(
+                                RectF(
+                                    location.left * scaleFactorX,
+                                    location.top * scaleFactorY,
+                                    location.right * scaleFactorX,
+                                    location.bottom * scaleFactorY
+                                ), paint
+                            )
+                            paint.style = Paint.Style.FILL
+                            val textToShow = "$category: ${String.format("%.2f", score)}" // Concatenate category and score
+                            canvas.drawText(
+                                textToShow,
+                                location.left * w,
+                                (location.top * h) - paint.textSize, // Adjust the vertical position of the text
+                                paint
+                            )
+                        }
+                    }
+                }
+
+                imageView.setImageBitmap(mutable)
+            }
+        }
+
+        cameraManager = getSystemService(Context.CAMERA_SERVICE) as CameraManager
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        model.close()
+    }
+
+    @SuppressLint("MissingPermission")
+    fun open_camera() {
+        cameraManager.openCamera(cameraManager.cameraIdList[0], object : CameraDevice.StateCallback() {
+            override fun onOpened(p0: CameraDevice) {
+                cameraDevice = p0
+
+                var surfaceTexture = textureView.surfaceTexture
+                var surface = Surface(surfaceTexture)
+
+                var captureRequest = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW)
+                captureRequest.addTarget(surface)
+
+                // Adjust the frame rate by setting the target FPS
+                captureRequest.set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, Range(15, 90))
+
+                cameraDevice.createCaptureSession(listOf(surface), object : CameraCaptureSession.StateCallback() {
+                    override fun onConfigured(p0: CameraCaptureSession) {
+                        p0.setRepeatingRequest(captureRequest.build(), null, null)
+                    }
+
+                    override fun onConfigureFailed(p0: CameraCaptureSession) {
+                    }
+                }, handler)
+            }
+
+            override fun onDisconnected(p0: CameraDevice) {
+
+            }
+
+            override fun onError(p0: CameraDevice, p1: Int) {
+
+            }
+        }, handler)
+    }
+
+    fun get_permission() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(android.Manifest.permission.CAMERA), 101)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+            get_permission()
+        }
+    }
+}
